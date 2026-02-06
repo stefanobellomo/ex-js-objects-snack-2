2a140062462713c4985ae2e88b119db1f5efb671
stefanobellomo <stefanobellomo1998@gmail.com>
2026-02-06 11:07:28 +0100

    snack 2

--- server.js
+++ server.js
@@ -2,5 +2,9 @@
 
 // Viene stampato Double Cheese Burger due volte ma l'oggetto è sempre uno poichè il tipo è reference
 
+// SNACK 2
+
+// Gli ingredienti vengono presi come reference e quindi viene sempre stampato salad, ma vengono creati due oggetti
+
 
 
